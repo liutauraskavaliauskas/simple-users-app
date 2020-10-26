65cf96fbb40b9df1ed464529802d1ce416d609bb
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-26 22:49:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
 <?php
+
+require_once 'autoload.php';
+
+use App\Controller\IndexController;
+use App\Controller\LoginController;
+use App\Database\Repository\UserRepository;
+
+$userName = $_POST['username'] ?? null;
+$password = $_POST['password'] ?? null;
+
+if (null !== $userName && null !== $password) {
+    $controller = new LoginController(new UserRepository());
+
+    $redirect = $controller->authenticate();
+
+    header($redirect);
+}
+
+$controller = new IndexController();
+
+$controller->index();
