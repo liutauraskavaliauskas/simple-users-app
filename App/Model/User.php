c5a4f0e2963cc6964d8d3cd33467b4e08829e917
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-26 22:48:06 +0200

    Update user model

--- App/Model/User.php
+++ App/Model/User.php
@@ -34,9 +34,19 @@ class User implements UserInterface
         return $this->id;
     }
 
+    public function setId(?string $id): void
+    {
+        $this->id = $id;
+    }
+
     public function getEmail(): ?string
     {
         return $this->email;
+    }
+
+    public function setEmail(?string $email): void
+    {
+        $this->email = $email;
     }
 
     public function getUserName(): ?string
@@ -44,13 +54,41 @@ class User implements UserInterface
         return $this->userName;
     }
 
+    public function setUserName(?string $userName): void
+    {
+        $this->userName = $userName;
+    }
+
     public function getPassword(): ?string
     {
         return $this->password;
     }
 
+    public function setPassword(?string $password): void
+    {
+        $this->password = $password;
+    }
+
     public function getCreatedAt(): ?\DateTime
     {
         return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public static function assign(array $data): User
+    {
+        $user = new self();
+
+        $user->setId($data['id'] ?? null);
+        $user->setEmail($data['email'] ?? null);
+        $user->setUserName($data['user_name'] ?? null);
+        $user->setPassword($data['password'] ?? null);
+        $user->setCreatedAt(isset($data['created_at']) ? new \DateTime($data['created_at']) : null);
+
+        return $user;
     }
 }
