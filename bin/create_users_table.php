c2f4bc7a0797ca20fe695ca9c18e118762c55d9f
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-27 15:39:07 +0200

    Update user table create script

--- bin/create_users_table.php
+++ bin/create_users_table.php
@@ -9,11 +9,12 @@ try {
     $connection = $database->getConnection();
 
     $connection->exec(
-        'CREATE TABLE IF NOT EXISTS users (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  email varchar(255) NOT NULL UNIQUE ,
-  password varchar(255) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        'CREATE TABLE IF NOT EXISTS user (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+email varchar(255) NOT NULL UNIQUE ,
+password varchar(255) NOT NULL,
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
     );
 
