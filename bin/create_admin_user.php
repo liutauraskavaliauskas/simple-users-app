cb7db5af21916656cdc97263df63524b7a1d230e
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-27 09:21:52 +0200

    Add admin user create

---
+++ bin/create_admin_user.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once '../autoload.php';
+
+use App\Database\Database;
+
+try {
+    $database = new Database();
+    $connection = $database->getConnection();
+
+    // default test password is: liutauras
+    // password_hash('liutauras', PASSWORD_BCRYPT) === '$2y$10$CsmdbrUH8K.0RQZJjKh5Q.4obRL8/e6y7XmUWVT0jTTTfRrmLdNm2'
+
+    $connection->exec(
+        'INSERT INTO users (email, password) 
+VALUES ("liutauras@liutauras.com", "$2y$10$CsmdbrUH8K.0RQZJjKh5Q.4obRL8/e6y7XmUWVT0jTTTfRrmLdNm2")
+ON DUPLICATE KEY UPDATE email = email'
+    );
+
+    $database->closeConnection();
+} catch (PDOException $exception) {
+    echo sprintf('Got exception: %s', $exception->getMessage());
+}
