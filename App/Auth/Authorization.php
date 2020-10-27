2de9c60ace2498d6911ea5c33bd63f66ec563176
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-27 17:44:00 +0200

    Update authorization context

--- App/Auth/Authorization.php
+++ App/Auth/Authorization.php
@@ -8,18 +8,17 @@ class Authorization
 {
     public static function canUserAccess(string $permission): void
     {
-        $user = $_SESSION['currentUser'] ?? null;
-
-        if (!$user instanceof User) {
-            die ('No logged in user found!');
-        }
-
-        if (true !== $user->hasPermission($permission)) {
+        if (true !== self::userHasPermission($permission)) {
             die ('You don\'t have permission for this action!');
         }
     }
 
     public static function userHasPermission(string $permission): bool
+    {
+        return self::getCurrentUser()->hasPermission($permission);
+    }
+
+    private static function getCurrentUser(): User
     {
         $user = $_SESSION['currentUser'] ?? null;
 
@@ -27,6 +26,6 @@ class Authorization
             die ('No logged in user found!');
         }
 
-        return $user->hasPermission($permission);
+        return $user;
     }
 }
