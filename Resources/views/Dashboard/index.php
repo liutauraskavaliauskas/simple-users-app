3ee0c7bc7f589688199ac6dc73b3af19e435ceb4
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-27 14:25:11 +0200

    Display users on dashboard

--- Resources/views/Dashboard/index.php
+++ Resources/views/Dashboard/index.php
@@ -1,3 +1,12 @@
+<?php
+
+require_once '../../../autoload.php';
+
+use App\Database\Repository\UserRepository;
+
+$users = (new UserRepository())->getAll();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +14,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" type="image/png" href="Resources/assets/images/icons/favicon.ico"/>
+    <link rel="icon" type="image/png" href="../../assets/images/icons/favicon.ico"/>
 
-    <link rel="stylesheet" type="text/css" href="Resources/assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/vendor/animate/animate.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/vendor/css-hamburgers/hamburgers.min.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/vendor/animsition/css/animsition.min.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/vendor/select2/select2.min.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/vendor/daterangepicker/daterangepicker.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/css/util.css">
-    <link rel="stylesheet" type="text/css" href="Resources/assets/css/main.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/util.css">
+    <link rel="stylesheet" type="text/css" href="../../assets/css/main.css">
 </head>
 <body>
 
@@ -33,35 +42,36 @@
                 </thead>
                 <tbody>
                 <?php
-                /**
-                 * @var int $key
-                 * @var \App\Model\User $user
-                 */
                 foreach ($users as $key => $user) {
                 ?>
                     <tr>
-                        <th scope="row"><?php echo ++$key; ?></th>
-                        <td><?php echo $user->getEmail() ?></td>
-                        <td><?php echo $user->getCreatedAt()->format('Y-m-d H:i:s')?></td>
+                        <th scope="row"><?php
+                            echo ++$key; ?></th>
+                        <td><?php
+                            echo $user->getEmail() ?></td>
+                        <td><?php
+                            echo $user->getCreatedAt()->format('Y-m-d H:i:s') ?></td>
                     </tr>
                 <?php
                 }
                 ?>
                 </tbody>
             </table>
+
+            <a href="newUser.php" class="btn btn-primary">Add new user</a>
         </div>
     </div>
 </div>
 
-<script src="Resources/assets/vendor/jquery/jquery-3.2.1.min.js"></script>
-<script src="Resources/assets/vendor/animsition/js/animsition.min.js"></script>
-<script src="Resources/assets/vendor/bootstrap/js/popper.js"></script>
-<script src="Resources/assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="Resources/assets/vendor/select2/select2.min.js"></script>
-<script src="Resources/assets/vendor/daterangepicker/moment.min.js"></script>
-<script src="Resources/assets/vendor/daterangepicker/daterangepicker.js"></script>
-<script src="Resources/assets/vendor/countdowntime/countdowntime.js"></script>
-<script src="Resources/assets/js/main.js"></script>
+<script src="../../assets/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="../../assets/vendor/animsition/js/animsition.min.js"></script>
+<script src="../../assets/vendor/bootstrap/js/popper.js"></script>
+<script src="../../assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="../../assets/vendor/select2/select2.min.js"></script>
+<script src="../../assets/vendor/daterangepicker/moment.min.js"></script>
+<script src="../../assets/vendor/daterangepicker/daterangepicker.js"></script>
+<script src="../../assets/vendor/countdowntime/countdowntime.js"></script>
+<script src="../../assets/js/main.js"></script>
 
 </body>
 </html>
