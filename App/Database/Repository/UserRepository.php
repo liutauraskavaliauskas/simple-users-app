63ef9df64d84ad1d78ea53ae582a7516a7e902ad
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-27 09:23:56 +0200

    Update user repository

--- App/Database/Repository/UserRepository.php
+++ App/Database/Repository/UserRepository.php
@@ -4,26 +4,59 @@ namespace App\Database\Repository;
 
 use App\Database\Database;
 use App\Model\User;
+use PDO;
 
 class UserRepository extends Database
 {
-    public function getOneByUserNameAndPassword(string $email, string $password): ?User
+    public function getOneByEmailAndPassword(string $email, string $password): ?User
     {
         $connection = $this->getConnection();
 
         $user = null;
 
-        if ($statement = $connection->prepare('SELECT * FROM users WHERE email = ? AND password = ?')) {
+        if ($statement = $connection->prepare('SELECT * FROM users WHERE email = :email')) {
             $statement->bindParam('email', $email);
-            $statement->bindParam('password', $password);
 
             $statement->execute();
 
-            $user = User::assign($statement->fetch());
+            $fetchedUser = $statement->fetch(PDO::FETCH_ASSOC);
+
+            if (false !== $fetchedUser) {
+                $validPassword = password_verify($password, $fetchedUser['password']);
+
+                if (true === $validPassword) {
+                    $user = User::assign($fetchedUser);
+                }
+            }
         }
+
 
         $this->closeConnection();
 
         return $user;
+    }
+
+    /**
+     * @return User[]
+     */
+    public function getAll(): array
+    {
+        $connection = $this->getConnection();
+
+        $users = [];
+
+        if ($statement = $connection->prepare('SELECT * FROM users')) {
+            $statement->execute();
+
+            $fetchedUsers = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+            foreach ($fetchedUsers as $fetchedUser) {
+                $users[] = User::assign($fetchedUser);
+            }
+        }
+
+        $this->closeConnection();
+
+        return $users;
     }
 }
