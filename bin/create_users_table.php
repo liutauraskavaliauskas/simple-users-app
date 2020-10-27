f519759722635b55c6cb786c0e02e35cce025692
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-27 09:21:41 +0200

    Update user table create

--- bin/create_users_table.php
+++ bin/create_users_table.php
@@ -11,7 +11,7 @@ try {
     $connection->exec(
         'CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  email varchar(255) NOT NULL,
+  email varchar(255) NOT NULL UNIQUE ,
   password varchar(255) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;'
