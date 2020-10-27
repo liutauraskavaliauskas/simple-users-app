1a1589705e48cdd405db6bde913116b440a60d4c
Liutauras Kavaliauskas <22213927+liutauraskavaliauskas@users.noreply.github.com>
2020-10-27 14:25:27 +0200

    Update dashboard controller

--- App/Controller/DashboardController.php
+++ App/Controller/DashboardController.php
@@ -2,24 +2,12 @@
 
 namespace App\Controller;
 
-use App\Database\Repository\UserRepository;
-
 class DashboardController
 {
-    /**
-     * @var UserRepository
-     */
-    private $userRepository;
-
-    public function __construct(UserRepository $userRepository)
-    {
-        $this->userRepository = $userRepository;
-    }
-
     public function index(): void
     {
-        $users = $this->userRepository->getAll();
+        header('Location: Resources/views/Dashboard/index.php');
 
-        include 'Resources/views/Dashboard/index.php';
+        exit();
     }
 }
